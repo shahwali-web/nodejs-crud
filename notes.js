56538026da48c7e0852bf00aa3e8bf4d50d94d36
Shah Wali <69451223+shahwalidjango@users.noreply.github.com>
2025-04-15 20:12:22 +0500

    for Node.js CRUD app

---
+++ notes.js
@@ -0,0 +1,64 @@
+import http from "http";
+import fs from "fs";
+
+//  we read data of notes.json file
+
+const readNote = () => {
+  const data = fs.readFileSync("notes.json");
+  return JSON.parse(data);
+};
+
+// Now post, create, or write data into NOtes.json file
+const writeNote = (notes) => {
+  fs.writeFileSync("notes.json", JSON.stringify(notes, null, 2));
+};
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/notes" && method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body = body + chunk;
+    });
+
+    req.on("end", () => {
+      const { title, content } = JSON.parse(body);
+      const notes = readNote();
+      const newNote = {
+        id: (notes.length + 1).toString(),
+        title,
+        content,
+      };
+
+      notes.push(newNote);
+      writeNote(notes);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(newNote));
+    });
+  } else if (url.startsWith("/notes/") && method == "GET") {
+    const id = url.split("/")[2];
+    const notes = readNote();
+    const note = notes.find((n) => n.id == id);
+
+    if (note) {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(note));
+    } else {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      res.end(JSON.stringify("Sorry Data is not fount! "));
+    }
+  } else if (url === "/notes" && method == "GET") {
+    const notes = readNote();
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify(notes));
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify("Sorry no data"));
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server is Running!");
+});
